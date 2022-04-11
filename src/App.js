a98db1330375f0f0cd25945f2b332015dc2c35c2
Michael Miller <millergm30@gmail.com>
2022-04-11 09:28:01 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data,setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=774150fdf98db0b4a2d36904c6b51a62`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid={APIKEY}`;
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
