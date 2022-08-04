60f8cdb7ec7a6d37bef919d2caa55a1bea410671
Michael Miller <millergm30@gmail.com>
2022-08-04 10:31:19 -0230

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,67 +1,68 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
 
-  const [data,setData] = useState({})
-  const [location, setLocation] = useState('')
-  const apiKey = '70b69f4f454f972394599b4936b6da57';
+  const [data,setData] = useState({});
+  const [location, setLocation] = useState("")
+  const apiKey = "70b69f4f454f972394599b4936b6da57";
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
 
   const searchLocation = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       axios.get(url).then((response) => {
         setData(response.data);
         console.log(response.data);
       })
-      setLocation('')
+      setLocation("")
     } 
-  }
+  };
 
   return (
-    <div className='App'>
-      <div className='search'>
+    <div className="relative bg-black/40 text-[#fff] h-screen bg-hero-pattern bg-no-repeat bg-center bg-cover">
+      <div className="text-center p-1">
         <input
+          className="px-1 py-2 text-lg rounded-xl border-2 border-solid border-white/80 bg-white/10 outline-none text-[#f8f8f8] placeholder:text-[#f8f8f8]"
           value={location}
           onChange={(event) => setLocation(event.target.value)}
           onKeyPress={searchLocation}
-          placeholder='Enter City'
-          type='text'
+          placeholder="Enter City"
+          type="text"
         />
       </div>
-      <div className='container'>
-        <div className='top'>
-          <div className='location'>
-            <p>{data.name}</p>
+      <div className="m-auto py-1 relative top-1 flex flex-col justify-between max-w-2xl h-[90%]">
+        <div className="w-full px-2 my-auto">
+          <div className="location">
+            <p className="text-lg">{data.name}</p>
           </div>
-          <div className='temp'>
-            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+          <div className="temp">
+            {data.main ? <h1 className="text-8xl">{data.main.temp.toFixed()}°C</h1> : null}
           </div>
-          <div className='description'>
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-            {data.weather ? <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt='' /> : null}
+          <div className="text-right">
+            {data.weather ? <p className="text-lg">{data.weather[0].main}</p> : null}
+            {data.weather ? <img className="w-14 inline-block" src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="" /> : null}
           </div>
         </div>
         {data.name !== undefined && (
-          <div className='bottom'>
-            <div className='feels'>
+          <div className="flex justify-evenly text-center w-full m-auto p-4 rounded-xl bg-black/20">
+            <div className="feels">
               {data.main ? (
-                <p className='bold'>{data.main.feels_like.toFixed()}°C</p>
+                <p className="text-lg font-bold">{data.main.feels_like.toFixed()}°C</p>
               ) : null}
-              <p>Feels Like</p>
+              <p className="text-lg">Feels Like</p>
             </div>
-            <div className='humidity'>
+            <div className="humidity">
               {data.main ? (
-                <p className='bold'>{data.main.humidity} %</p>
+                <p className="text-lg font-bold">{data.main.humidity} %</p>
               ) : null}
-              <p>Humidity</p>
+              <p className="text-lg">Humidity</p>
             </div>
-            <div className='wind'>
+            <div className="wind">
               {data.wind ? (
-                <p className='bold'>{data.wind.speed.toFixed()} KM/h</p>
+                <p className="text-lg font-bold">{data.wind.speed.toFixed()} KM/h</p>
               ) : null}
-              <p>Wind Speed</p>
+              <p className="text-lg">Wind Speed</p>
             </div>
           </div>
         )}
